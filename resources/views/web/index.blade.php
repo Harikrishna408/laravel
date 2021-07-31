9379a3050b5aaad42c6c4de8d5828d3da51c901d
Harikrishna408 <harikrishnaece1@gmail.com>
2021-07-31 19:29:54 +0530

    first commit

---
+++ resources/views/web/index.blade.php
@@ -0,0 +1,50 @@
+@extends('web.layout')
+@section('content')
+       <!-- End Header Content -->
+
+       <!-- NOTIFICATION CONTENT -->
+         @include('web.common.notifications')
+      <!-- END NOTIFICATION CONTENT -->
+
+       <!-- Carousel Content -->
+       <?php  echo $final_theme['carousel']; ?>
+       <!-- Fixed Carousel Content -->
+
+      <!-- Banners Content -->
+      <!-- Products content -->
+
+      <?php
+
+      $product_section_orders = json_decode($final_theme['product_section_order'], true);
+      foreach ($product_section_orders as $product_section_order){
+          if($product_section_order['status'] == 1)
+          {
+            //echo $product_section_order['file_name'].'<br>';
+            $r =   'web.product-sections.' . $product_section_order['file_name'];
+      
+      ?>
+            @include($r)
+      
+      <?php
+
+          }
+       }
+      
+      ?>
+
+@php
+$product_section_orders = json_decode($final_theme['product_section_order'], true);
+@endphp
+
+@foreach($product_section_orders as $product_section_order)
+@if($product_section_order['status'] == 1)
+	@php
+		$r = 'web.product-sections.' . $product_section_order['file_name'];
+	@endphp
+	@include($r)
+@endif
+@endforeach
+
+@include('web.common.scripts.addToCompare')
+@include('web.common.scripts.Like')
+@endsection
